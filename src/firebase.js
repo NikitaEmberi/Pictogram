89c72f184aea34d6ec72e60fd85d96dca48b4fd8
NikitaEmberi <59737567+NikitaEmberi@users.noreply.github.com>
2021-11-09 20:10:37 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,24 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDgg1mKFDkpsCylAvqz4WJD8W1lOnlMmTo",
-//   authDomain: "instagram-clone-354a3.firebaseapp.com",
-//   projectId: "instagram-clone-354a3",
-//   storageBucket: "instagram-clone-354a3.appspot.com",
-//   messagingSenderId: "215928978336",
-//   appId: "1:215928978336:web:bf15c8cbdfe556498682c5",
-//   measurementId: "G-GSSE9FFZFY"
-// };
-
-// Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
 
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/auth';
@@ -26,13 +8,13 @@ import 'firebase/compat/firestore';
 import 'firebase/compat/storage';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyDgg1mKFDkpsCylAvqz4WJD8W1lOnlMmTo",
-    authDomain: "instagram-clone-354a3.firebaseapp.com",
-    projectId: "instagram-clone-354a3",
-    storageBucket: "instagram-clone-354a3.appspot.com",
-    messagingSenderId: "215928978336",
-    appId: "1:215928978336:web:bf15c8cbdfe556498682c5",
-    measurementId: "G-GSSE9FFZFY"  
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""  
 });
 
 const db = firebaseApp.firestore();
